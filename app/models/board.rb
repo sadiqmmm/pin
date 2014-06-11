a5b4a0b44e1396ab9b45ff6951f834fc160c1b39
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-06-11 21:18:09 +0530

    Add Friendly id with conf to board and pin

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,7 @@
 class Board < ActiveRecord::Base
+	extend FriendlyId
+	firendly_id :title, use: :slugged
+	
 	belongs_to :users
 	has_many :pins
 end
---
+++ db/migrate/20140611153355_add_slug_to_board.rb
@@ -0,0 +1,6 @@
+class AddSlugToBoard < ActiveRecord::Migration
+  def change
+    add_column :boards, :slug, :string
+    add_index :boards, :slug, unique: true
+  end
+end
